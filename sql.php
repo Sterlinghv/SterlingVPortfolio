edc0a46025f44b41c15a597e7066a810947577ab
Sterlinghv <stervioo@gmail.com>
2021-09-20 21:00:15 -0600

    First commit

---
+++ sql.php
@@ -0,0 +1,19 @@
+<?php
+
+$database_ip = "sterbase.cjn6tcmx4ory.us-east-2.rds.amazonaws.com";
+$username = "admin";
+$password = "oneal4587";
+
+$link = mysqli_connect($database_ip, $username, $password, "name of db");
+
+if (mysqli_connect_errno()) {
+    echo "failed " . mysqli_connect_error();
+    exit();
+}
+
+$_POST['txusername']
+$sql=insert into 
+$_POST['comment1']
+
+//echo "connection successful"
+?>
